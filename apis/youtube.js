beb224f6c9b8f06cc717afb274e8ccd0ccdcc86c
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2020-08-22 17:52:20 +0530

    Update youtube.js

--- apis/youtube.js
+++ apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const KEY  = 'AIzaSyBB4yQEXNOBCnScb3BrD6vw-OYQ18Fa9Yw';
+const KEY  = '';
 
 export default axios.create({
     baseURL : 'https://www.googleapis.com/youtube/v3',
